45b478031f4a89522aebbf8ff94e8c453042e59d
shivani <shi9114@ccs.neu.edu>
2015-04-29 17:16:05 -0400

    Fixed bugs.

--- server.js
+++ server.js
@@ -258,12 +258,9 @@ app.get("/company/:id", function(req, res){
 //------------------------------------------------------------------------------------------------------------
 
 app.get("/jobByCId/:id", function(req, res){
-	console.log("entered");
 	 if(req.params.id){
-	 	console.log(req.params.id);
 	 	Job.find({ companyId: req.params.id }, function (err, docs){
 	 		res.json(docs);
-	 		console.log(docs);
 	 	})
 	 }
 })
@@ -468,7 +465,6 @@ app.post('/logout', function(req, res)
     req.logOut();
     res.send(200);
 });     
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
